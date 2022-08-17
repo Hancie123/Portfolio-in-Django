81beba57492cb5f6654bd3900f5bd562fa7c6b08
Hancie Phago <Hanciewanemphago@gmail.com>
2022-08-17 09:26:45 +0545

    second commit

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from home import views
+from django.conf.urls.static import static
 
 
 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,8 +115,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+import os
+
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS= [
+
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
